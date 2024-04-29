0b04951142576a710830fcff57c3eb73481250f5
True1Santony <espartaconassekine@gmail.com>
2024-04-29 21:28:38 +0200

    itext Reports v5

---
+++ iTextReports/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace iTextReports
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnGeneraInforme = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(555, 373);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnGeneraInforme
+            // 
+            this.btnGeneraInforme.Location = new System.Drawing.Point(611, 362);
+            this.btnGeneraInforme.Name = "btnGeneraInforme";
+            this.btnGeneraInforme.Size = new System.Drawing.Size(75, 23);
+            this.btnGeneraInforme.TabIndex = 1;
+            this.btnGeneraInforme.Text = "Informe";
+            this.btnGeneraInforme.UseVisualStyleBackColor = true;
+            this.btnGeneraInforme.Click += new System.EventHandler(this.btnGeneraInforme_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 409);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(713, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 431);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnGeneraInforme);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnGeneraInforme;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ iTextReports/Form1.cs
@@ -0,0 +1,168 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iTextReports
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection oleConection;
+        private DataSet dataSet;
+        private const String DATA_BASE_PATH = "Data Source=Z:\\DAM\\2 Desarrollo de interfaces\\TEMA 2\\2.1\\Nwind.mdb";
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += Form1_FormClosing;//antes de finalizar el programa lanza FormClosing
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           oleConection = ConnectDataBase(DATA_BASE_PATH);
+
+            String sql = "SELECT p.ProductName, p.QuantityPerUnit, p.UnitPrice, p.UnitsInStock, c.CategoryName " +
+                                        "FROM Products p " +
+                                        "INNER JOIN Categories c ON p.CategoryID = c.CategoryID; ";
+
+            dataGridView1.DataSource = ReturnDataTable(sql);
+            
+        }
+
+
+        /// <summary>
+        /// Crea una conexion a ddbb a partir de un string path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>OleDbConnection</returns>
+        public OleDbConnection ConnectDataBase(String path)
+        {
+            OleDbConnection oleDbConnection = new OleDbConnection();
+            oleDbConnection.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0; " + path;
+            oleDbConnection.Open();
+
+            toolStripStatusLabel1.Text = "Base de datos abierta";
+
+            return oleDbConnection;
+        }
+
+        /// <summary>
+        /// Método que devuelve un data table de una consulta sql
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns>DataTable</returns>
+        public DataTable ReturnDataTable(String sql)
+        {
+
+            OleDbCommand oleDbCommand = new OleDbCommand();
+            oleDbCommand.Connection = oleConection;
+            oleDbCommand.CommandText = sql;
+
+            OleDbDataAdapter oleDbDataAdapter = new OleDbDataAdapter();
+            OleDbCommandBuilder oleDbCommandBuilder = new OleDbCommandBuilder(oleDbDataAdapter);
+            oleDbDataAdapter.SelectCommand = oleDbCommand;
+            oleDbDataAdapter.FillSchema(dataSet = new DataSet(), SchemaType.Mapped, "productos");
+            oleDbDataAdapter.Fill(dataSet, "productos");
+
+            DataTable dataTable = dataSet.Tables["productos"];
+            return dataTable;
+        }
+
+        /// <summary>
+        /// Metodo Click del boton Informe, recoge los datos del DataGridView y los transforma en pdf exportable
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnGeneraInforme_Click(object sender, EventArgs e)
+        {
+
+            if (dataGridView1.Rows.Count > 0)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "PDF (*.pdf)|*.pdf";
+                sfd.FileName = "Output.pdf";
+                bool fileError = false;
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    if (File.Exists(sfd.FileName))
+                    {
+                        try
+                        {
+                            File.Delete(sfd.FileName);
+                        }
+                        catch (IOException ex)
+                        {
+                            fileError = true;
+                            MessageBox.Show("It wasn't possible to write the data to the disk." + ex.Message);
+                        }
+                    }
+                    if (!fileError)
+                    {
+                        try
+                        {
+                            PdfPTable pdfTable = new PdfPTable(dataGridView1.Columns.Count);
+                            pdfTable.DefaultCell.Padding = 3;
+                            pdfTable.WidthPercentage = 100;
+                            pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+
+                            foreach (DataGridViewColumn column in dataGridView1.Columns)
+                            {
+                                PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText));
+                                pdfTable.AddCell(cell);
+                            }
+
+                            foreach (DataGridViewRow row in dataGridView1.Rows)
+                            {
+                                foreach (DataGridViewCell cell in row.Cells)
+                                {
+                                    if (cell.Value != null) pdfTable.AddCell(cell.Value.ToString());
+                                }
+                            }
+
+                            using (FileStream stream = new FileStream(sfd.FileName, FileMode.Create))
+                            {
+                                Document pdfDoc = new Document(PageSize.A4, 10f, 20f, 20f, 10f);
+                                PdfWriter.GetInstance(pdfDoc, stream);
+                                pdfDoc.Open();
+                                pdfDoc.Add(pdfTable);
+                                pdfDoc.Close();
+                                stream.Close();
+                            }
+
+                            toolStripStatusLabel1.Text = "Informe generado!!!";
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error :" + ex.Message);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No Record To Export !!!", "Info");
+            }
+
+           
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            if (oleConection != null && oleConection.State == ConnectionState.Open)
+            {
+                oleConection.Close();
+                
+            }
+
+        }
+    }
+}
